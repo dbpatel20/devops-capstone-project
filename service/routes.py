98a8236c78cd11d5078a0fbfcc674683ee2e4a73
Darshil Patel <darshilpatel2020@gmail.com>
2025-05-04 13:30:23 -0400

    changes

--- service/routes.py
+++ service/routes.py
@@ -86,15 +86,17 @@ def read_account(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
+    """
+    Update an Account
+    """
+    app.logger.info("Request to update account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id {account_id} not found")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id '{account_id}' was not found.")
 
     account.deserialize(request.get_json())
     account.update()
     return jsonify(account.serialize()), status.HTTP_200_OK
-
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
